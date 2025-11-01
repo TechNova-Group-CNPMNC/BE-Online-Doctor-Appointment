04ea81fd3ae70893da8adbee1eb6bc7dd2644d3f
QuanTran <tranminhquan.411@gmail.com>
2025-11-01 13:07:53 +0700

    config CORS

--- src/main/java/com/assignment/clinic/config/SecurityConfig.java
+++ src/main/java/com/assignment/clinic/config/SecurityConfig.java
@@ -19,9 +19,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(
-                            "http://localhost:5173"
-                    ));
+                    config.setAllowedOrigins(List.of("*"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
